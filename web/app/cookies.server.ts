2ea3193200e75c1f956f49fb9841641c48809ee3
Alex Hicks <alex@alexhicks.net>
2024-07-05 00:01:24 -0400

    Set auth cookie max age to 1 week

--- web/app/cookies.server.ts
+++ web/app/cookies.server.ts
@@ -1,7 +1,9 @@
 import { IFullAuthTokenFragment } from "@aldahick/rex-sdk";
 import { createCookie } from "@remix-run/node";
 
-export const authCookie = createCookie("rex:authToken");
+export const authCookie = createCookie("rex:authToken", {
+  maxAge: 7 * 24 * 60 * 60,
+});
 export const getCookieAuth = (
   request: Request,
 ): Promise<IFullAuthTokenFragment | null> =>
